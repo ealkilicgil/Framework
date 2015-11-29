930b66e385d286aaca61d856546153c2289f5811
ealkilicgil <ealkilicgil@gmail.com>
2015-11-29 20:21:42 +0200

    Initial commit

---
+++ app/src/main/java/es/framework/es/framework/entities/TodoItem.java
@@ -0,0 +1,54 @@
+package es.framework.es.framework.entities;
+
+/**
+ * Created by 02481552 on 29.11.2015.
+ */
+public class TodoItem {
+    private Long id;
+    private String title;
+    private boolean checked;
+    private Long dateCreated;
+    private Long dateModified;
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isChecked() {
+        return checked;
+    }
+
+    public void setChecked(boolean checked) {
+        this.checked = checked;
+    }
+
+    public Long getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(Long dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public Long getDateModified() {
+        return dateModified;
+    }
+
+    public void setDateModified(Long dateModified) {
+        this.dateModified = dateModified;
+    }
+
+
+}
