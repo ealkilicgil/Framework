5cc164b40418d714f7f6cc35769a4b9420d04bc7
ealkilicgil <ealkilicgil@gmail.com>
2015-11-29 20:21:24 +0200

    Initial commit

---
+++ app/src/main/java/es/framework/es/framework/entities/Note.java
@@ -0,0 +1,62 @@
+package es.framework.es.framework.entities;
+
+/**
+ * Created by 02481552 on 29.11.2015.
+ */
+public class Note {
+    private Long id;
+    private String title;
+    private String content;
+    private int color;
+    private Long dateCreated;
+    private Long dateModified;
+    public Long getDateModified() {
+        return dateModified;
+    }
+
+    public void setDateModified(Long dateModified) {
+        this.dateModified = dateModified;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public int getColor() {
+        return color;
+    }
+
+    public void setColor(int color) {
+        this.color = color;
+    }
+
+    public Long getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(Long dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+
+}
